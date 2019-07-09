f834827b18c6d0c9d5fcd2a4b77cc372c44e6d5d
Marek Hewelt <22530856+lightflicker@users.noreply.github.com>
2019-07-09 18:27:23 +0100

    Added moonca.py with next ingress and voc calcs

---
+++ flatlib/moonca.py
@@ -0,0 +1,97 @@
+from . import const
+from .chart import Chart
+from .geopos import GeoPos
+from .datetime import Datetime
+
+"""Calculate a minute in Julian Calendar"""
+MINUTE = const.MINUTE
+DIST = [0, 2, 3, 4, 6, 8, 9, 10]
+
+def NextMoonIngress(chart):
+    """Define the sign angles"""
+    angles = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360]
+
+    # Initate local variables
+    # The next sign angle from the current moon position
+    angle = 0.0
+    # Distance between the current Moon position and the next sign
+    dist = 0.0
+    # An error between the new Moon position and the sign angle
+    a_err = 10.0
+    # Intiate iteration counter for statistical purposes
+    i = 0
+
+    # Load the current Moon position
+    moon = chart.get(const.MOON)
+
+    # Find the next sign angle
+    for ang in angles:
+        if moon.lon < ang:
+            angle = ang
+            dist = ang - moon.lon
+            break
+
+    # Evalute a time in day before the next Moon ingress
+    # Days to the next ingress
+    dti = (dist / const.MEAN_MOTION_MOON)
+
+    # Add the DTI into the the current Moon position
+    tme = Datetime.fromJD(chart.date.jd + dti,0)
+
+    # Evaluate progressed chart
+    p_chart = Chart(tme,chart.pos)
+    p_moon = p_chart.get(const.MOON)
+    a_err = angle - p_moon.lon
+    # Run the loop for calculating the ingress time
+    while (abs(a_err) > 0.0001):
+        p_chart = Chart(tme,chart.pos)
+        p_moon = p_chart.get(const.MOON)
+        a_err = angle - p_moon.lon
+        scale = 100.0 * abs(a_err)
+
+        if a_err > 0.0:
+            tme = Datetime.fromJD(tme.jd + (MINUTE * scale),0)
+        if a_err == 0.0:
+            break
+        if a_err < 0.0:
+            tme = Datetime.fromJD(tme.jd - (MINUTE * scale),0)
+        i += 1
+    return {'Date':tme,'Error':a_err,'Iter':i}
+
+
+def isCA(chart):
+    """Is canceling aspect present?"""
+    IDate = NextMoonIngress(chart)
+    p_chart = Chart(IDate['Date'],chart.pos)
+    moon = chart.get(const.MOON)
+    obj = 0
+    obj_lon = 0
+    asp_type = 0
+
+    for ob in p_chart.objects:
+        if ob.id in const.LIST_ASP_PLANETS:
+            dist = abs(const.LIST_SIGNS.index(moon.sign) - const.LIST_SIGNS.index(ob.sign))
+            if dist in DIST:
+                if ob.signlon > obj_lon:
+                    obj_lon = ob.signlon
+                    obj = ob
+                    if dist == 0:
+                        if obj.id in [const.SUN,
+                                const.MERCURY,
+                                const.VENUS,
+                                const.JUPITER,
+                                const.NEPTUNE]:    #Conjunction
+                            asp_type = 1
+                        else:
+                            asp_type = 0
+                    if dist == 2 or dist == 10: #Sextile
+                        asp_type = 1
+                    if dist == 3 or dist == 9:  #Square
+                        asp_type = 0
+                    if dist == 4 or dist == 8:  #Trine
+                        asp_type = 1
+                    if dist == 6:               #Opposition
+                        asp_type = 0
+
+  
+    return {'Last obj':obj.id,'Lon':obj.signlon,'POS':asp_type,'NI':IDate['Date']}
